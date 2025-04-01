ab9309f4344e3bababdc846a9d83b190b63eadae
Bruna Borges <brunaborgesdev@gmail.com>
2025-04-01 07:49:58 -0300

    upload exercices

--- js-steps/ex1/script.js
+++ js-steps/ex1/script.js
@@ -1,41 +1,24 @@
-/*
-Desafio guiado 4: calculaServico
-Cria uma função chamada calculaServico que recebe dois parâmetros:
+const servicos = [
+  { nome: "corte", preco: 30 },
+  { nome: "barba", preco: 20 },
+  { nome: "combo", preco: 45 }
+];
 
-servico: string com o nome do serviço ("corte", "barba" ou "combo")
-
-ehVip: boolean dizendo se o cliente é VIP
-
-A função deve:
-
-Definir o preço do serviço:
-
-corte = 30
-
-barba = 20
-
-combo = 45
-
-Se for VIP, aplicar 10% de desconto
-
-Mostrar no console: "Serviço: corte | Valor final: R$ 27.00" (com o valor certinho)*/
-
-function calculaServico (servico, ehVip) {
-  let ehVip = false;
-  servico = [
-    {
-      nome: "corte",
-      preco: 30,
-    },
-    {
-      nome: "barba",
-      preco: 20,
-    },
-    {
-      nome: "combo",
-      preco: 45,
-    }
-  ]
-
+function calculaServico(servicoEscolhido, ehVip) {
   
+  const servico = servicos.find(servico => servico.nome === servicoEscolhido);
+  if(!servico){
+    console.log("Serviço não encontrado");
+    return;
+  }
+
+  if (ehVip) {
+    const desconto = servico.preco * 0.2;
+    const precoDescontado = servico.preco - desconto;
+    console.log(`O preço do ${servicoEscolhido} é de ${precoDescontado}`)
+  } else {
+    console.log(`Nao ha desconto para o serviço ${servicoEscolhido}`)
+  }
 }
+
+calculaServico("corte", true);
